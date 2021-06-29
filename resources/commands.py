a68e943f18587c14502a95465b0d62e43d4ed3f7
Coolo22 <39219877+Coolo22@users.noreply.github.com>
2021-06-29 19:42:03 +0100

    Add files via upload

---
+++ resources/commands.py
@@ -0,0 +1,9 @@
+json = {
+    "moderation":["ban", "kick", "warn", "warns", "delwarn", "purge", "mute", "unmute", "createinvite", "role", "nick"],
+    "setup":["prefix", "dashboard"],
+    "information":["help", "botinfo", "ping", "links", "changelogs"],
+    "fun":["deepfry", "blurpify", "colorfilter", "dadjoke", "fact", "clap", "reddit", "achievement", "whiteboard", "rip", "cat", "dog"],
+    "utility":["randomnumber", "google", "wikipedia", "userinfo", "serverinfo", "serverstats", "convert", "avatar", "embed", "poll", "question", "covid", "remind", "timer"],
+    "economy":["work", "extrawork", "flip", "roll", "leaderboard"],
+    "misc":["suggest", "report"]
+}
