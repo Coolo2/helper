083a7d2fdaa2e85f570d3a69b49c848af64b329b
Coolo22 <39219877+Coolo22@users.noreply.github.com>
2021-06-29 19:41:46 +0100

    Add files via upload

---
+++ setup/var.py
@@ -0,0 +1,20 @@
+
+musicCommands = ["connect", "play", "pause", "stop", "resume", "skip", "queue", "now_playing", "volume", "musicping", "join", "summon", "shut", "q", "playlist", "np", "current", "currentsong", "playingnow", "playing", "vol", "leave", "disconnect", "die", "musicuptime", "loop"]
+
+embed=0xFF8700
+embedSuccess=0x00FF00
+embedFail=0xFF0000
+
+version = "1.0.0"
+prefix="hbt/"
+owner=368071242189897728
+
+address = "http://192.168.0.40:5000"
+login = f"https://discord.com/api/oauth2/authorize?client_id=444882566529417216&redirect_uri={address}/login&response_type=code&scope=identify%20guilds"
+
+invite = "https://discord.com/api/oauth2/authorize?client_id=486180321444888586&permissions=8&scope=bot"
+server = "https://discord.gg/HChmbSN"
+topgg = "https://top.gg/bot/486180321444888586"
+website = address
+
+botAdmins = [368071242189897728, 444176530357354527, 449662597927665666]
